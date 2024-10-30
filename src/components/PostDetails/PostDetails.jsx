04e303c8ddab95fd0abbb2d3136c070d98c6fc0d
imon-sheikh <mdimonhossain2017@gmail.com>
2024-10-30 11:50:35 +0600

    added go back funtionality using useNavigate()

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,15 +1,22 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PostDetails = () => {
 
     const post = useLoaderData()
+    const navigate = useNavigate()
+
     const {id,title,body} = post
+
+    const handleGoBack = () => {
+         navigate(-1)
+    }
 
     return (
         <div>
             <h1>I am from post details: {id}</h1>
             <p>Title: {title}</p>
             <p><small>{body}</small></p>
+            <button onClick={handleGoBack}>Go back</button>
         </div>
     );
 };
